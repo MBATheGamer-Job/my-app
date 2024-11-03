ad8d9fe5912c7769d0c9736b9b82f4de592604a9
MBATheGamer-Job <mbathegamer+job@gmail.com>
2024-11-03 23:13:53 +0100

    setup TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+import daisyui from "daisyui";
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    daisyui
+  ],
+}
+
